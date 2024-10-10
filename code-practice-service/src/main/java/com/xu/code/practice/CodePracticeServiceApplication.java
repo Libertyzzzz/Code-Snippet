92cbe2a4ae93b45e4bcee49a06ecaa65fe6f542c
cyril <1424537137@qq.com>
2024-10-10 22:08:49 +0800

    init commit

---
+++ code-practice-service/src/main/java/com/xu/code/practice/CodePracticeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.xu.code.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePracticeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePracticeServiceApplication.class, args);
+    }
+
+}
---
+++ code-practice-service/src/main/java/com/xu/code/practice/entity/GeneralEntity.java
@@ -0,0 +1,11 @@
+package com.xu.code.practice.entity;
+
+public class GeneralEntity {
+
+    /**
+     * @Author liberty
+     * @Date 2024/10/10 22:06
+     */
+
+
+}
